dd7045dc43b3887f8893381e4f902a2237f261fb
carlosvldz <me@carlosvldz.com>
2021-03-01 14:44:09 -0600

    add Exercises page to refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Card from "./components/Card";
-import Welcome from "./components/Welcome";
+import Exercises from "./pages/Exercises";
 import "bootstrap/dist/css/bootstrap.css";
 
 const container = document.getElementById("root");
 
-ReactDOM.render(
-  <div>
-    <Welcome username="Carlos" />
-    <Card
-      title="Technique Guide"
-      description="Learn amazing street workout and calisthenics"
-      img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
-      leftColor="#a74cf2"
-      rightColor="#617bfb"
-    />
-  </div>,
-  container
-);
+ReactDOM.render(<Exercises />, container);
